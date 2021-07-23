7f6c8ff5129b91212ad7a4033f9ef6fd0a9aaa41
MauriiAC <mauriciocuello91@gmail.com>
2021-07-22 23:41:05 -0300

    main function done

---
+++ src/components/HoverRating.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Rating from "@material-ui/lab/Rating";
+import Box from "@material-ui/core/Box";
+
+const labels = {
+  0.5: "0.5 / 5",
+  1: " 1 / 5 ",
+  1.5: " 1.5 / 5 ",
+  2: " 2 / 5 ",
+  2.5: " 2.5 / 5 ",
+  3: " 3 / 5 ",
+  3.5: " 3.5 / 5 ",
+  4: " 4 / 5 ",
+  4.5: " 4.5 / 5 ",
+  5: " 5 / 5 "
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+    display: "flex",
+    alignItems: "center"
+  }
+});
+
+export default function HoverRating( { actualValue } ) {
+  
+    const [value, setValue] = React.useState(actualValue);
+    const [hover, setHover] = React.useState(-1);
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+        <Rating
+            name="hover-feedback"
+            value={value}
+            precision={0.5}
+            onChange={(event, newValue) => {
+            setValue(newValue);
+            }}
+            onChangeActive={(event, newHover) => {
+            setHover(newHover);
+            }}
+        />
+        {value !== null && (
+            <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>
+        )}
+        </div>
+    );
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,66 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { searchParkings } from '../redux/actions';
+import { searchParkings, cleanErr } from '../redux/actions';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(5, 2),
+      width: '25ch',
+    },
+  },
+}));
 
 const SearchBar = () => {
 
+    const classes = useStyles();
     const dispatch = useDispatch();
     const [input, setInput] = useState('');
-
-
+    
+    function handleSearch(e){
+        dispatch(cleanErr())
+        dispatch(searchParkings(input))
+    }
+    
     function handleChange(e){
         setInput(e.target.value)
     }
-
-    function handleSearch(e){
+    
+    function handleSubmit(e){
+        e.preventDefault()
+        dispatch(cleanErr())
         dispatch(searchParkings(input))
     }
-
+    
     return (
         <>
-            <input
-                value={input}
-                onChange={handleChange}
-            />
-            <button
-                onClick={handleSearch}
+            <form 
+                className={classes.root}
+                noValidate 
+                autoComplete="off"
+                onSubmit={handleSubmit}
             >
-                Search
-            </button>
+                <TextField 
+                    id="outlined-basic" 
+                    label="City"
+                    variant="outlined"
+                    value={input}
+                    onChange={handleChange}
+                    style={{ height: "56px", width: "30%" , color: 'white' }}
+                />
+                <Button
+                    variant="contained" 
+                    color="primary"
+                    onClick={handleSearch}
+                    style={{ height: "56px", width: "90px" }}
+                >
+                    Search
+                </Button>
+            </form>
 
         </>
     )
